8d64f823f74f2076afbbc0111c2fcb28a474b54f
Yash Bansal <ph-yash@mobiloitte.com>
2020-08-04 19:17:17 +0530

    Integrating reactive forms validation with css fix and validation messages in the sction 4 page and Integrating reactive forms validation with css fix and validation messages in the sction 4 page, Integrating angular material in the project of representative panel

--- src/app/component/application-form/section4/section4.component.ts
+++ src/app/component/application-form/section4/section4.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'app-section4',
@@ -9,17 +9,55 @@ import { FormGroup } from '@angular/forms';
 export class Section4Component implements OnInit {
 
   section4Form:FormGroup;
-    
-  constructor() { }
+  qualificationArr: FormArray;
+
+
+  constructor( private fb: FormBuilder) { 
+    this.section4Form = this.fb.group({
+      stateHighestQualification : new FormControl(),
+      achieved : new FormControl(),
+      qualificationArr : this.fb.array([])
+    });
+    this.qualificationArr = this.section4Form.get('qualificationArr') as FormArray;
+    this.qualificationArr.push(this.createCall(null,null,null,null,null,null,null,null,null,null));
+  }
 
   ngOnInit() {
     window.scrollTo(0, 0);
   }
 
   saveAndQuit(){
-    if(this.section4Form.invalid){
-        return false
-    }
+    // if(this.section4Form.invalid){
+    //     return false
+    // }
     console.log("value-->",this.section4Form.value)
+  }
+
+  createCall(country,qualification,resultGrade,subject1,subject1Grade,subject2,subject2Grade,institutionName,startDate,endDate): FormGroup {
+    return this.fb.group({
+      country  : new FormControl(country),
+      qualification : new FormControl(qualification),
+      resultGrade : new FormControl(resultGrade),
+      subject1 : new FormControl(subject1),
+      subject1Grade : new FormControl(subject1Grade),
+      subject2 : new FormControl(subject2),
+      subject2Grade : new FormControl(subject2Grade),
+      institutionName : new FormControl(institutionName),
+      startDate : new FormControl(startDate),
+      endDate : new FormControl(endDate),
+    });
+  }
+
+  addQualification() : void {
+    this.qualificationArr = this.section4Form.get('qualificationArr') as FormArray;
+    this.qualificationArr.push(this.createCall(null,null,null,null,null,null,null,null,null,null));
+    console.log("form data-->",this.qualificationArr.value)
+  }
+
+  cancel() {
+    this.section4Form = this.fb.group({
+      qualificationArr: this.fb.array([])
+    });
+    this.addQualification()
   }
 }
--- src/app/component/application-form/section5/section5.component.ts
+++ src/app/component/application-form/section5/section5.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-section5',
@@ -7,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Section5Component implements OnInit {
 
+  section5Form :FormGroup;
+
   constructor() { }
 
   ngOnInit() {
     window.scrollTo(0, 0);
+    this.initializeForm()
   }
+
+  initializeForm(){
+      this.section5Form = new FormGroup({
+          "nativeOfEnglishSpeakingCountry" : new FormControl(),
+          "nativeEnglishSpeakingCountryName" : new FormControl(),
+          "englishLanguageTestDetails" : new FormControl(),
+          "professionalQualification" : new FormControl(),
+          "englishQualificationName" : new FormControl(),
+          "englishQualificationDate" : new FormControl(),
+          "englishQualificationScore1" : new FormControl(),
+          "englishQualificationScore2" : new FormControl(),
+          "englishQualificationScore3" : new FormControl(),
+          "englishQualificationScore4" : new FormControl(),
+          "highestAcedemicQualification" : new FormControl(),
+          "typeOfEnglishQualification" : new FormControl(),
+          "resultType" : new FormControl(),
+          "overallResult" : new FormControl(),
+      })
+  }
+
+  saveAndQuit(){
+    console.log("save---->>",this.section5Form.value)
+  }
+
+  selectNativeOfEnglishSpeakingCountry(event){
+    console.log('vj--->',event)
+  }
+
 
 }
