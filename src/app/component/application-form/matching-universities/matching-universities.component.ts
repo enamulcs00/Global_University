9a7e8556491b17ddb04a074277c0fabb792ac672
Yash Bansal <ph-yash@mobiloitte.com>
2020-08-12 19:09:12 +0530

    Implementing the saving data feature for incomplete forms and form editing forms for the application form 15 page,Integrate the form validation with validation message in html in application form 15 page, Integrate the get country list api in matching university page, Integrate the mat autocomplete in html of matching university page, Integrate the api of  get university list with the filter of country name & bind the data in the html in matching university page

--- src/app/component/application-form/matching-universities/matching-universities.component.ts
+++ src/app/component/application-form/matching-universities/matching-universities.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicesService } from 'src/app/services.service';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 
+declare var $ : any;
 @Component({
   selector: 'app-matching-universities',
   templateUrl: './matching-universities.component.html',
@@ -7,10 +12,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchingUniversitiesComponent implements OnInit {
 
-  constructor() { }
+  countryList: any = []
+  universityList:any = []
+  myControl = new FormControl();
+  filteredOptions: Observable<string[]>;
+  countryName:any= ''
+  selectall:boolean = false
+  constructor(private service: ServicesService) { }
 
   ngOnInit() {
     window.scrollTo(0, 0);
+    this.service.getCountryStates()
+        .subscribe((data) => {
+           this.countryList = data
+            this.filteredOptions = this.myControl.valueChanges
+              .pipe(
+                  startWith(''),
+                  map(value => this._filter(value))
+              );
+        }, (error) => {
+    });
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.countryList.filter(option => option.country.toLowerCase().includes(filterValue));
+  }
+
+  selectCountry(){
+    // console.log("-->",this.myControl.value)
+  }
+
+  reset(){
+    this.countryName = '';
+  }
+
+  getUniversity(){
+    this.countryName = this.myControl.value
+    this.service.getApi(`university/v1.1/web/get-university-by-country-for-most-popular-university?country=${this.countryName}&page=0&pageSize=1000`,2).subscribe((res :any ) => {
+      console.log("res--->",res)
+      this.selectall = false
+      this.universityList = []
+      if(res.status == 200 && res.body.data.searchData){
+        res.body.data.searchData.forEach(element => {
+          this.universityList.push({ 
+            selected : false,
+            ...element
+          })
+        });
+      }
+    })
+  }
+
+  selectAll(event){
+    this.universityList.forEach(element => {
+      element.selected = event.srcElement.checked
+    });
+  }
+
+  select(event,index){
+    this.universityList[index].selected = event.srcElement.checked
+  }
+
+  submit(){
+    console.log("-->>",this.universityList)
+    // [routerLink]="['/dashboard']"
+    $('#exampleModalCenter').modal('show')
   }
 
 }
