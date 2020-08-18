43a94320a56b4a06741a91df3531af46e0ae5bf6
Yash Bansal <ph-yash@mobiloitte.com>
2020-08-18 21:15:49 +0530

    Integrate the filter-user-details api in the  manage users page in the representative panel,Integrate the ui of the mange users page

--- src/app/component/profile/manage-users/manage-users.component.ts
+++ src/app/component/profile/manage-users/manage-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicesService } from 'src/app/services.service';
 
 @Component({
   selector: 'app-manage-users',
@@ -7,13 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageUsersComponent implements OnInit {
   accountData: any;
+  userList :any
 
-  constructor() { }
+  constructor(private service:ServicesService) { }
 
   ngOnInit() {
     window.scroll(0,0)
     this.accountData = JSON.parse(localStorage.getItem('myProfile'))
     this.accountData.imageUrl = this.accountData.imageUrl ? this.accountData.imageUrl : 'assets/images/pick-1.png';
+    this.getUserList()
+  }
+
+  getUserList(){
+    this.userList = []
+    this.service.getApi(`account/admin/user-management/filter-user-details`,1).subscribe((res:any) => {
+      console.log('res-->>',res)
+      this.userList = res.body.data.list
+    })
   }
 
 }
